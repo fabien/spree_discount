4e88828d8b93b5e95c30cd7479bb53294f9f3191
Fabien Franzen <info@atelierfabien.be>
2012-09-03 18:24:35 +0200

    Use Spree::Money

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -18,7 +18,7 @@ Spree::Variant.class_eval do
   
   alias_method(:variant_options_hash_without_discount, :variant_options_hash) unless method_defined?(:variant_options_hash_without_discount)
   def variant_options_hash
-    hsh = reduced_price? ? { :pre_discount_price => number_to_currency(self.pre_discount_price) } : {}
+    hsh = reduced_price? ? { :pre_discount_price => Spree::Money.new(self.pre_discount_price).to_s } : {}
     variant_options_hash_without_discount.merge(hsh)
   end
   
